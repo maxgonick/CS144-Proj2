2e4aa78409a6be80394c9600d38975af32c7bc4c
maxgonick <maxwellgonick@g.ucla.edu>
2024-05-04 09:46:58 -0700

    drag and drop functionality and styling

--- public/card.js
+++ public/card.js
@@ -10,18 +10,15 @@ export default class Card {
 
   addToCol(colElem, mover) {
     //TODO
+    let foo = new Set();
     //Helper Functions
     const deleteEvent = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
       console.log("WORK");
       mover.stopMoving();
       const cardNode = event.target.closest(".card");
       cardNode.remove();
     };
     const leaveEditEvent = (event) => {
-      event.stopPropagation();
-      event.preventDefault();
       const cardNode = event.target.closest(".card");
       const editField = cardNode.querySelector(".editDescription");
       console.log(editField.value);
@@ -29,8 +26,6 @@ export default class Card {
       editField.classList.add("hidden");
     };
     const editEvent = (event) => {
-      event.stopPropagation();
-      event.preventDefault();
       const cardNode = event.target.closest(".card");
       const editField = cardNode.querySelector(".editDescription");
       editField.classList.remove("hidden");
@@ -39,11 +34,37 @@ export default class Card {
       editField.select();
     };
     const moveEvent = (event) => {
-      event.stopPropagation();
-      event.preventDefault();
+      console.log(event.target, " saw this event");
       mover.stopMoving();
       console.log(event.target.closest(".card"));
       mover.startMoving(event.target.closest(".card"));
+    };
+    const dropEvent = (event) => {
+      event.preventDefault();
+      const eventCard = event.currentTarget;
+      if (event.dataTransfer.items) {
+        const reader = new FileReader();
+        const file = event.dataTransfer.items[0].getAsFile();
+        reader.onload = () => {
+          eventCard.querySelector(".description").textContent = reader.result;
+        };
+        reader.readAsText(file);
+      }
+      event.currentTarget.classList.remove("dragoverZone");
+    };
+    const dragenterEvent = (event) => {
+      event.preventDefault();
+      foo.add(event.target);
+      console.log("enter");
+      event.currentTarget.classList.add("dragoverZone");
+    };
+    const dragleaveEvent = (event) => {
+      event.preventDefault();
+      foo.delete(event.target);
+      console.log("leave", event.target, this);
+      if (foo.size === 0) {
+        event.currentTarget.classList.remove("dragoverZone");
+      }
     };
     const newElement = document.querySelector(".template").cloneNode(true);
     newElement.classList.remove("template");
@@ -53,9 +74,15 @@ export default class Card {
     newElement.querySelector(".delete").addEventListener("click", deleteEvent);
     newElement.querySelector(".edit").addEventListener("click", editEvent);
     newElement.querySelector(".editDescription").addEventListener("blur", leaveEditEvent);
-    newElement.querySelector(".startMove").addEventListener("click", moveEvent);
+    newElement.querySelector(".startMove").addEventListener("mousedown", moveEvent);
     newElement.querySelector(".description").innerText = NO_DESCRIPTION_TEXT;
     self.card = newElement;
+    newElement.addEventListener("dragenter", dragenterEvent);
+    newElement.addEventListener("dragleave", dragleaveEvent);
+    newElement.addEventListener("drop", dropEvent);
+    newElement.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
     colElem.append(newElement);
   }
   setDescription(text) {
